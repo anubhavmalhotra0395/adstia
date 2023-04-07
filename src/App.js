395d35f04e9f39a596b023189469dacdb23e0a52
anubhavmalhotra0395 <malhotraanubhav0395@gmail.com>
2023-04-07 23:45:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
+import Carousel from './Carousel';
 import './App.css';
+import Support from './support.png'
+import Conversions from './conversions.png'
+import Quality from './quality.png'
+import BullsEye from './bullseye.png'
+import Campaigns from './campaigns.png'
+import { useMediaQuery } from 'react-responsive'
 
 function App() {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 624px)' })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='inner-block1'>
+        <div className='inner-block1-1'>
+          <p className='header2'>Why Choose Us?</p>
+          <h3 className='header1'>Why the Industry Chooses Clickdee?</h3>
+          <p className='text1'>We understand performance marketing from every angle and every stage of the funnel. Our clients trust that we know what metrics move their business towards growth.Our publisher and affiliate partners know that we make maximum revenue and ROAS a main focus when growing our partnerships</p>
+        </div>
+        <div className='inner-block1-2'>
+          <img src={BullsEye} className='inner-img'/>
+          <h3 className='text2'>Choose Your Local Targeting</h3> 
+            <div id="hover-content">
+              <p>Our targeted and tracked calls are tailored to your business needs, audience and geolocation.</p>
+      </div>
+        </div>
+        <div className='inner-block1-3'>
+          <img src={Conversions} className='inner-img'/>
+          <h3 className='text2'>Track Your Conversion</h3>
+          <div id="hover-content">
+       <p>We use the most progressive and analytics technology to ensure all calls can be tracked and evaluated</p>
+    </div>
+        </div>
+      </div>
+      <div className='inner-block2'>
+        <div className='inner-block1-4'></div>
+        <div className='inner-block1-2'>
+        <img src={Campaigns} className='inner-img'/>
+          <h3 className='text2'>Customized Campaigns</h3>
+          <div id="hover-content">
+        <p>Audiences, budgets and goals: you decide the criteria for your digital ad campaign, and we'll do the rest</p>
+    </div>
+        </div>
+        <div className='inner-block1-3'>
+        <img src={Support} className='inner-img'/>
+          <h3 className='text2'>Get Dedicated Support Team</h3>
+          <div id="hover-content">
+          <p>We constantly monitor quality control! Our affiliate network of publishers are digital ad experts, that have been vetted</p>
+        </div>
+        </div>
+        <div className='inner-block1-2'>
+        <img src={Quality} className='inner-img'/>
+          <h3 className='text2'>Quality Assurance</h3>
+          <div id="hover-content">
+          <p>We constantly monitor quality control! Our affiliate network of publishers are digital ad experts, that have been vetted</p>
+    </div>
+        </div>
+      </div>
+      {isTabletOrMobile && <Carousel/>}
     </div>
   );
 }
---
+++ src/Carousel.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import './Carousel.css';
+import './App.css'
+import Support from './support.png'
+import Conversions from './conversions.png'
+import Quality from './quality.png'
+import BullsEye from './bullseye.png'
+import Campaigns from './campaigns.png'
+
+function Carousel() {
+  const [scrollPos, setScrollPos] = useState(0);
+
+  useEffect(() => {
+    function handleScroll() {
+      setScrollPos(window.pageYOffset+300);     
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const items = [
+    {
+      id: 1,
+      color: '#fbfbfb',
+      text:'Choose Your Local Targeting',
+      text2:'Our targeted and tracked calls are tailored to your business needs, audience and geolocation.',
+      image:BullsEye
+    },
+       {
+      id: 2,
+      color: '#fbfbfb',
+      text:'Choose Your Local Targeting',
+      text2:'Our targeted and tracked calls are tailored to your business needs, audience and geolocation.',
+      image:Conversions,
+    },
+       {
+      id: 3,
+      color: '#fbfbfb',
+      text:'Choose Your Local Targeting',
+      text2:'Our targeted and tracked calls are tailored to your business needs, audience and geolocation.',
+      image:Campaigns
+    },
+       {
+      id: 4,
+      color: '#fbfbfb',
+      text:'Choose Your Local Targeting',
+      text2:'Our targeted and tracked calls are tailored to your business needs, audience and geolocation.',
+      image:Support
+    },
+       {
+      id: 5,
+      color: '#fbfbfb',
+      text:'Choose Your Local Targeting',
+      text2:'Our targeted and tracked calls are tailored to your business needs, audience and geolocation.',
+      image:Quality
+    },
+     
+    
+  ];
+
+  return (
+    <>
+    <div className='hero'></div>
+ 
+    <div className="carousel-wrapper">
+      <div
+        className="carousel"
+        style={{transform: `translateX(-${scrollPos}px)`  }}
+      >
+        {items.map((item) => (
+          <div key={item.id} className="item" style={{ backgroundColor: item.color}}>
+          <img src={item.image} className='mob-img'/>
+          <h5 className='head1'>{item.text}</h5>
+          <p className='head2'>{item.text2}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+   <div style={{backgroundColor:'#4682B4',marginTop:'-370px',display:'flex',flexDirection:'column',padding:'20px',height:'500px'}}>
+    <h3 style={{fontSize:'30px',color:'white',textAlign:'start'}}>Work Together</h3>
+    <p style={{fontSize:'25px',color:'white',marginTop:'-10px'}}>Lets All make it work Together</p>
+    <p style={{color:'white',fontSize:'16px'}}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. </p>
+   </div>
+    </>
+  );
+}
+
+export default Carousel;
